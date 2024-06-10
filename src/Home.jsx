3d197c2dab584a2ad994ef70fe261618471db768
unknown <ocoten321@gmail.com>
2024-06-10 10:15:47 +0700

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -75,20 +75,20 @@ function Home() {
                 </div>
                 <h1>12</h1>
             </div>
-            {/* <div className='card'>
+            <div className='card'>
                 <div className='card-inner'>
-                    <h3>CUSTOMERS</h3>
+                    <h3>Tổng tiền người chơi đã rút</h3>
                     <BsPeopleFill className='card_icon'/>
                 </div>
                 <h1>33</h1>
             </div>
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>ALERTS</h3>
-                    <BsFillBellFill className='card_icon'/>
-                </div>
-                <h1>42</h1>
-            </div> */}
+            {/*<div className='card'>*/}
+            {/*    <div className='card-inner'>*/}
+            {/*        <h3>ALERTS</h3>*/}
+            {/*        <BsFillBellFill className='card_icon'/>*/}
+            {/*    </div>*/}
+            {/*    <h1>42</h1>*/}
+            {/*</div> */}
         </div>
 
         <div className='charts'>
